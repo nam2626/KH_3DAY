12e96bfef1c3ae8895609fa9cb5ff4c3daded97c
nam2626 <nam2626@gmail.com>
2025-10-15 11:30:52 +0900

    클래스-1

---
+++ java/Step01/src/e01_class/Person.java
@@ -0,0 +1,21 @@
+package e01_class;
+
+public class Person {
+	//클래스 안에 선언된 변수는 해당 객체가 일을 할 때 사용되는 변수
+	//이런 변수들을 필드(field) 라고 함.
+	
+	//접근제어자
+	//	private default protected public
+	
+	//필드
+	//이름
+	private String name;
+	//나이
+	private int age;
+	
+	//메서드
+	// 객체가 할일(우리가 시킬일)
+	public void printInfo() {
+		System.out.println(name + " / " + age);
+	}
+}
--- java/Step01/src/print/HelloWorld.java
+++ java/Step01/src/print/HelloWorld.java
@@ -1,3 +1,4 @@
+package print;
 
 public class HelloWorld {
 
