acd499731851b2e649edfe87134975e45fc84214
nam2626 <nam2626@gmail.com>
2025-10-16 11:16:14 +0900

    has a 관계

--- java/Step02/src/e02_has_a/Army.java
+++ java/Step02/src/e02_has_a/Army.java
@@ -1,6 +1,25 @@
 package e02_has_a;
 
 public class Army {
+	private Gun gun;
+
+	public Army(Gun gun) {
+		this.gun = gun;
+	}
 	
+	public void changeGun(Gun gun) {
+		this.gun = gun;
+	}
+	
+	public void shot() {
+		gun.shot();
+	}
+	
+	public void reload() {
+		gun.reload();
+	}
 
 }
+
+
+
--- java/Step02/src/e02_has_a/HasAMain.java
+++ java/Step02/src/e02_has_a/HasAMain.java
@@ -3,10 +3,11 @@ package e02_has_a;
 public class HasAMain {
 
 	public static void main(String[] args) {
-		Army army = new Army();
+		Army army = new Army(new Gun());
 		for (int i = 0; i < 20; i++) {
 			army.shot();
 		}
+		// 총기를 교체하고 쏘는 부분
 	}
 
 }
