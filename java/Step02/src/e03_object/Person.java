a174e00ffee700f75e39137c3bac8c2468553c71
nam2626 <nam2626@gmail.com>
2025-10-16 11:52:44 +0900

    Object - toString

--- java/Step02/src/e03_object/Person.java
+++ java/Step02/src/e03_object/Person.java
@@ -1,5 +1,7 @@
 package e03_object;
 
+import java.util.Objects;
+
 public class Person {
 	private String name;
 	private int age;
@@ -8,7 +10,7 @@ public class Person {
 		this.name = name;
 		this.age = age;
 	}
-	
+
 	/*
 	 * 객체가 가지고 있는 필드나 메서드의 결과를
 	 * 외부에서 쉽게 확인이 가능하게
@@ -16,6 +18,17 @@ public class Person {
 	 * 
 	 * 재정의하지 않으면 타입명@객체의해시코드(16진수)
 	*/
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", age=" + age + "]";
+	}
+	
+	@Override
+	public int hashCode() {
+//		return name.hashCode() + age;
+		return Objects.hash(name,age);
+	}
+	
 }
 
 
--- java/Step02/src/e03_object/PersonMain.java
+++ java/Step02/src/e03_object/PersonMain.java
@@ -3,7 +3,20 @@ package e03_object;
 public class PersonMain {
 
 	public static void main(String[] args) {
+		Person p = new Person("홍길동", 20);
+		Person s = new Person("홍길동", 20);
 		
+		System.out.println(p.hashCode());
+		System.out.println(s.hashCode());
+		
+		//내용은 같지만 메모리 주소가 달라서 false가 나온다
+		System.out.println(p);
+		System.out.println(s);
+		System.out.println(p == s);
 	}
 
 }
+
+
+
+
