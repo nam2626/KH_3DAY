6a7e5fc7730e562fda19d21faa851c2d35c2f9a8
nam2626 <nam2626@gmail.com>
2025-10-16 16:40:19 +0900

    리스트 예제

--- java/Step02/src/e07_list/ListMain.java
+++ java/Step02/src/e07_list/ListMain.java
@@ -5,35 +5,43 @@ import java.util.ArrayList;
 public class ListMain {
 	
 	//ArrayList, LinkedList, Vector
+	//데이터를 추가한 순서대로 저장, 중복된 데이터를 저장할 수 있음
 	public static void main(String[] args) {
-		ArrayList<Person> arrayList = new ArrayList<Person>();
+		ArrayList<Person> list = new ArrayList<Person>();
 		
 		//데이터 추가
-		arrayList.add(new Person("김철수", 22));
-		arrayList.add(new Person("박영수", 21));
-		arrayList.add(new Person("문규수", 32));
-		arrayList.add(new Person("이철수", 33));
-		arrayList.add(new Person("헝철수", 35));
-		arrayList.add(new Person("강명수", 44));
+		list.add(new Person("김철수", 22));
+		list.add(new Person("박영수", 21));
+		list.add(new Person("문규수", 32));
+		list.add(new Person("이철수", 33));
+		list.add(new Person("헝철수", 35));
+		list.add(new Person("강명수", 44));
 		
-		System.out.println(arrayList);
+		System.out.println(list);
 		
 		//데이터 삭제
-		arrayList.remove(5);
-		System.out.println(arrayList);
-		arrayList.remove(new Person("문규수", 32));
-		System.out.println(arrayList);
+		list.remove(5);
+		System.out.println(list);
+		list.remove(new Person("문규수", 32));
+		System.out.println(list);
 		
 		//데이터가 저장된 개수
-		System.out.println("저장된 값의 개수 : "+arrayList.size());
+		System.out.println("저장된 값의 개수 : "+list.size());
 		
 		//데이터 교체
+		list.set(0,new Person("오수호", 44));
+		System.out.println(list);
+		list.add(0, new Person("임강호", 33));
+		System.out.println(list);
 		
 		//특정 데이터가 있는지 확인?
-		
+		System.out.println(list.contains(new Person("헝철수", 35)));
 		//특정 데이터가 있는 위치
+		System.out.println(list.indexOf(new Person("헝철수", 35)));
 		
 		//전체 초기화
+		//list.clear();
+		System.out.println(list);
 		
 		//데이터 전체 조회 방법 - 1
 		
