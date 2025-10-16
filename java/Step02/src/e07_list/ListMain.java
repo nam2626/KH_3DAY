e849082dc7bb6d3ad2baa411d6ff444dd351ea1a
nam2626 <nam2626@gmail.com>
2025-10-16 16:23:04 +0900

    리스트 예제

--- java/Step02/src/e07_list/ListMain.java
+++ java/Step02/src/e07_list/ListMain.java
@@ -3,11 +3,18 @@ package e07_list;
 import java.util.ArrayList;
 
 public class ListMain {
-
+	
+	//ArrayList, LinkedList, Vector
 	public static void main(String[] args) {
 		ArrayList<Person> arrayList = new ArrayList<Person>();
 		
 		//데이터 추가
+		arrayList.add(new Person("김철수", 22));
+		arrayList.add(new Person("박영수", 21));
+		arrayList.add(new Person("문규수", 32));
+		arrayList.add(new Person("이철수", 33));
+		arrayList.add(new Person("헝철수", 35));
+		arrayList.add(new Person("강명수", 44));
 		
 		//데이터 삭제
 		
