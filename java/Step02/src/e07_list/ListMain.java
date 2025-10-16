f5ad076721c874d30c5b904be7f94c89ea3ed86a
nam2626 <nam2626@gmail.com>
2025-10-16 15:51:39 +0900

    리스트 예제

---
+++ java/Step02/src/e07_list/ListMain.java
@@ -0,0 +1,11 @@
+package e07_list;
+
+import java.util.ArrayList;
+
+public class ListMain {
+
+	public static void main(String[] args) {
+		ArrayList<Person> arrayList = new ArrayList<Person>();
+	}
+
+}
---
+++ java/Step02/src/e07_list/Person.java
@@ -0,0 +1,38 @@
+package e07_list;
+
+import java.util.Objects;
+
+public class Person {
+	private String name;
+	private int age;
+	public Person(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", age=" + age + "]";
+	}
+	
+	@Override
+	public int hashCode() {
+		System.out.println("Person - Hash");
+		return Objects.hash(age, name);
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		System.out.println("Person - Equals");
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		return age == other.age && Objects.equals(name, other.name);
+	}
+	
+	
+}
