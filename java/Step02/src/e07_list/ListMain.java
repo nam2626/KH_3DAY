df4f27850066b51bb44151f730fe3c0474dc1c19
nam2626 <nam2626@gmail.com>
2025-10-16 16:12:21 +0900

    리스트 예제

--- java/Step02/src/e07_list/ListMain.java
+++ java/Step02/src/e07_list/ListMain.java
@@ -6,6 +6,33 @@ public class ListMain {
 
 	public static void main(String[] args) {
 		ArrayList<Person> arrayList = new ArrayList<Person>();
+		
+		//데이터 추가
+		
+		//데이터 삭제
+		
+		//데이터가 저장된 개수
+		
+		//데이터 교체
+		
+		//특정 데이터가 있는지 확인?
+		
+		//특정 데이터가 있는 위치
+		
+		//전체 초기화
+		
+		//데이터 전체 조회 방법 - 1
+		
+		//데이터 전체 조회 방법 - 2
+		
+		//데이터 전체 조회 방법 - 3
 	}
 
 }
+
+
+
+
+
+
+
