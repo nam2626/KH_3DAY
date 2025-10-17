18632aedcf1b46d0afa63e52d673ed05f8642848
nam2626 <nam2626@gmail.com>
2025-10-17 11:46:51 +0900

    윈도우 함수

--- sql/oracle_review.sql
+++ sql/oracle_review.sql
@@ -10,6 +10,12 @@ GRANT RESOURCE, CONNECT, CREATE VIEW TO C##ADMIN;
 -- 저장소 사용량 부여
 ALTER USER C##ADMIN DEFAULT TABLESPACE USERS QUOTA UNLIMITED ON USERS;  
 -----------------------------------------------------------------------------
+-- 데이터 타입
+-- 문자열 : CHAR(2000까지 지원), VARCHAR2(4000), CLOB(128TB)
+-- 숫자 : NUMBER(자리수, 소수점개수) -> 최대 38, FLOAT(128)
+-- 날짜 시간 : DATE(날짜/시간), TIMESTAMP(소수점까지 저장 가능 최대 9자리)
+-- https://docs.oracle.com/en/database/oracle/oracle-database/19/refrn/datatype-limits.html
+
 -- CAR
 -- 차량번호 - 문자 10개, 차량명, 제조사, 제조년도 - 정수 4자리, 금액 - 정수 5자리
 -- 차량번호 --> A000000000
@@ -36,12 +42,24 @@ CREATE TABLE CAR(
 	CONSTRAINT PK_CAR PRIMARY KEY(ID)
 );
 
-
 SELECT LENGTHB('바') FROM DUAL;
 
 SELECT * FROM USER_CONSTRAINTS;
 
+-- CAR 테이블 전체 조회
+SELECT * FROM CAR;
 
+-- 자동차 제조사만 조회
+SELECT DISTINCT MAKER FROM CAR;
+-- 제조사 데이터 건수 조회
+SELECT COUNT(*) FROM (SELECT DISTINCT MAKER FROM CAR);
+SELECT ROW_NUMBER() OVER(ORDER BY MAKER), MAKER FROM (SELECT DISTINCT MAKER FROM CAR);
+-- 제조사별 데이터 건수
+SELECT DISTINCT MAKER, COUNT(*) OVER(PARTITION BY MAKER)
+FROM CAR;
+-- 자동차의 제조사별 모델의 금액 순위
+SELECT ID, CNAME, MAKER, RANK() OVER(PARTITION BY MAKER ORDER BY PRICE)
+FROM CAR;
 
 
 
