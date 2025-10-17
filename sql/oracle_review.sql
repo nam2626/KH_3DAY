7e04f042059ac38d80b9c074cee34ad0bcb222af
nam2626 <nam2626@gmail.com>
2025-10-17 10:07:22 +0900

    계정 생성 및 사용량 지정

--- sql/oracle_review.sql
+++ sql/oracle_review.sql
@@ -2,5 +2,10 @@
 sqlplus sys/oracle as sysdba
 -- 관리자 비밀번호 변경
 alter user system identified by 123456;
---JDBC 드라이버 위치 : C:\app\user1\product\21c\dbhomeXE\jdbc\lib
-
+-- JDBC 드라이버 위치 : C:\app\user1\product\21c\dbhomeXE\jdbc\lib
+-- 사용자 생성
+CREATE USER C##ADMIN IDENTIFIED BY 123456;
+-- 권한 적용
+GRANT RESOURCE, CONNECT, CREATE VIEW TO C##ADMIN;
+-- 저장소 사용량 부여
+ALTER USER C##ADMIN DEFAULT TABLESPACE USERS QUOTA UNLIMITED ON USERS;  
