b72ed03f1f73ff9b46183e490c61c24baa59f750
nam2626 <nam2626@gmail.com>
2025-10-24 15:17:00 +0900

    세션 및 request 영역 데이터 출력

--- spring/spring-web/src/main/java/com/kh/controller/MainController.java
+++ spring/spring-web/src/main/java/com/kh/controller/MainController.java
@@ -2,6 +2,10 @@ package com.kh.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import com.kh.SpringWebApplication;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class MainController {
@@ -15,7 +19,34 @@ public class MainController {
 	public String loginView() {
 		return "login";
 	}
+	
+	@PostMapping("/member/login")
+	public String login(HttpServletRequest request,HttpSession session) {
+		String userName = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		System.out.println(userName + " / " + password);
+		
+		request.setAttribute("msg", "안녕하세요 : " + userName);
+		
+//		HttpSession session = request.getSession();
+		
+		session.setAttribute("pwd", "암호 : "+password);
+		
+		return "main";
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
