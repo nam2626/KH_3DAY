e7e868aab9d9674e0b3b1473dbd164d426fcc63e
nam2626 <nam2626@gmail.com>
2025-10-28 16:47:11 +0900

    export

--- react/step00/src/App.js
+++ react/step00/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-import { plus } from './func/math';
+import { minus, plus } from './func/math';
 
 function App() {
   let sum = plus(100, 200);
+  let sub = minus(40, 5);
   return (
     <div className="App">
       <h2>덧셈 결과 : {sum}</h2>
+      <h2>뺄셈 결과 : {sub}</h2>
       <p>안뇽 ㅋㅋㅋㅋ</p>
     </div>
   );
--- react/step00/src/func/math.js
+++ react/step00/src/func/math.js
@@ -4,3 +4,7 @@ export function plus(a, b) {
 }
 //뺄셈 결과 리턴하는 함수
 const minus = (a, b) => a - b;
+
+export { minus };
+// 한번에 export 가능
+// export { minus, plus };
