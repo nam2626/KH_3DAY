c53200a48c850df8522508c14324950e59404761
nam2626 <nam2626@gmail.com>
2025-10-28 17:09:24 +0900

    export default

--- react/step00/src/App.js
+++ react/step00/src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import { minus, plus } from './func/math';
+import mylog, { multi } from './func/log';
 
 function App() {
   let sum = plus(100, 200);
   let sub = minus(40, 5);
+  let mul = multi(5, 3);
+  mylog('우리가 만들 로그함수');
   return (
     <div className="App">
       <h2>덧셈 결과 : {sum}</h2>
       <h2>뺄셈 결과 : {sub}</h2>
+      <h2>곱셈 결과 : {mul}</h2>
       <p>안뇽 ㅋㅋㅋㅋ</p>
     </div>
   );
---
+++ react/step00/src/func/log.js
@@ -0,0 +1,6 @@
+export default function log(msg) {
+  console.log('log:', msg);
+}
+export function multi(a, b) {
+  return a * b;
+}
