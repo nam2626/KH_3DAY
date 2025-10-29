5538eed95685b5affe47cb14142b59fdb51de9a5
nam2626 <nam2626@gmail.com>
2025-10-29 11:18:38 +0900

    props

--- react/component-props/src/App.js
+++ react/component-props/src/App.js
@@ -1,10 +1,18 @@
 import ButtonOK from './components/ButtonOK';
 
 function App() {
+  const profile = [
+    { name: 'Alex', age: 20, job: 'developer' },
+    { name: 'Bob', age: 17, job: 'student' },
+    { name: 'Charlie', age: 25, job: 'designer' },
+    { name: 'David', age: 30, job: 'developer' },
+    { name: 'Ethan', age: 15, job: 'student' },
+  ];
+
   return (
     <div className="App">
       <h1>Component Props</h1>
-      <ButtonOK attr1="속성1" data="데이터" var="확인" />
+      <ButtonOK attr1="속성1" data="데이터" var1="확인" />
     </div>
   );
 }
--- react/component-props/src/components/ButtonOK.jsx
+++ react/component-props/src/components/ButtonOK.jsx
@@ -1,5 +1,11 @@
-export default (props) => {
-  console.log(props);
+// export default (props) => {
+export default ({ attr1, data, var1 }) => {
+  // console.log(props);
+  // const attr1 = props.attr1;
+  // const data = props.data;
+  // const var1 = props.var1;
+  // const { attr1, data, var1 } = props;
+  console.log(attr1, data, var1);
 
-  return <button>OK</button>;
+  return <button onClick={() => alert(`${attr1} / ${data} / ${var1}`)}>OK</button>;
 };
