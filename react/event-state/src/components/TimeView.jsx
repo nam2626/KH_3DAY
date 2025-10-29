c7770e7ac57e54562a52d3fe9971d70f7fd2460d
nam2626 <nam2626@gmail.com>
2025-10-29 14:10:35 +0900

    수정

--- react/event-state/src/components/TimeView.jsx
+++ react/event-state/src/components/TimeView.jsx
@@ -1,8 +1,16 @@
+import { useEffect, useState } from 'react';
+
 export default () => {
   console.log('TimeView 렌더링');
+  const [date, setDate] = useState(new Date());
+  setTimeout(() => {
+    setDate(new Date());
+    console.log(date.toLocaleString());
+  }, 1000);
   return (
     <>
       <h2>TimeView</h2>
+      <p>{date.toLocaleString()}</p>
     </>
   );
 };
