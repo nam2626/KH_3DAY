3fb322bf4243f5b09e1fc12d0768fd5be8135577
nam2626 <nam2626@gmail.com>
2025-10-29 11:07:10 +0900

    props

---
+++ react/component-props/src/App.js
@@ -0,0 +1,12 @@
+import ButtonOK from './components/ButtonOK';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Component Props</h1>
+      <ButtonOK attr1="속성1" data="데이터" var="확인" />
+    </div>
+  );
+}
+
+export default App;
---
+++ react/component-props/src/components/ButtonOK.jsx
@@ -0,0 +1,5 @@
+export default (props) => {
+  console.log(props);
+
+  return <button>OK</button>;
+};
