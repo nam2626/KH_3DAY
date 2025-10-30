d46b91411115d0998223fbdec3100916452c0c1d
nam2626 <nam2626@gmail.com>
2025-10-30 14:52:14 +0900

    reducer 적용

--- react/event-state/src/components/ReducerEx.jsx
+++ react/event-state/src/components/ReducerEx.jsx
@@ -1,23 +1,30 @@
-import { useReducer } from 'react';
+import { useReducer, useRef } from 'react';
 //상태값을 제어하는 함수
 function reducer(state, action) {
-  console.log(action);
+  console.log(state, action);
   switch (action.type) {
     case 'INCREMENT':
       return { value: state.value + 1 };
     case 'DECREMENT':
       return { value: state.value - 1 };
+    case 'ADDNUMBER':
+      return { value: state.value + action.payload };
   }
 }
 export default () => {
   const [count, dispatch] = useReducer(reducer, { value: 0 });
+  const num = useRef(null);
   return (
     <div>
       <h2>reducer 예제</h2>
       <p>count : {count.value}</p>
       <p>
         <button onClick={() => dispatch({ type: 'INCREMENT', payload: 1 })}>+</button>
-        <button>-</button>
+        <button onClick={() => dispatch({ type: 'DECREMENT', payload: 1 })}>-</button>
+      </p>
+      <p>
+        <input type="number" ref={num} />
+        <button onClick={() => dispatch({ type: 'ADDNUMBER', payload: parseInt(num.current.value) })}>추가</button>
       </p>
     </div>
   );
