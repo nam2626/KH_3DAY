8c1e83827d8d97470ce20068a46546e0ebd95d88
nam2626 <nam2626@gmail.com>
2025-10-31 14:49:21 +0900

    컨트롤러 생성

--- spring/member-app-back/src/main/java/com/kh/controller/MainController.java
+++ spring/member-app-back/src/main/java/com/kh/controller/MainController.java
@@ -1,5 +1,9 @@
 package com.kh.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kh.service.MemberService;
@@ -13,6 +17,23 @@ public class MainController {
 		this.service = service;
 	}
 	
+	@GetMapping("/member/list")
+	public Map<String, Object> memberList(){
+		Map<String, Object> map = new HashMap<String, Object>();
+		
+		//전체 회원정보 읽어와서 map에 저장
+				
+		
+		return map; 
+	}
 	
 
 }
+
+
+
+
+
+
+
+
