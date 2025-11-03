7ae046e938d5cc4758e1f07eb671d5cb5caa10de
nam2626 <nam2626@gmail.com>
2025-11-03 09:42:43 +0900

    로그인 페이지 까지 완성

---
+++ react/spring-member-app/src/pages/Login.jsx
@@ -0,0 +1,10 @@
+export default () => {
+  return (
+    <div>
+      <h2>로그인</h2>
+      <input type="text" placeholder="아이디 입력" />
+      <input type="password" placeholder="암호 입력" />
+      <button>로그인</button>
+    </div>
+  );
+};
