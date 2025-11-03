4c612dd2939303d0944608dc82ae2d89ab3dd106
nam2626 <nam2626@gmail.com>
2025-11-03 11:50:22 +0900

    디자인 적용 및 전체 회원정보 출력

--- react/spring-member-app/src/components/Header.jsx
+++ react/spring-member-app/src/components/Header.jsx
@@ -2,15 +2,21 @@ import { Link } from 'react-router-dom';
 
 export default () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/home">홈</Link>
-        </li>
-        <li>
-          <Link to="/register">회원등록</Link>
-        </li>
-      </ul>
+    <nav className="navbar navbar-expand-lg">
+      <div className="container-fluid">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link className="nav-link" to="/home">
+              홈
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/register">
+              회원등록
+            </Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
--- react/spring-member-app/src/pages/Main.jsx
+++ react/spring-member-app/src/pages/Main.jsx
@@ -1,10 +1,12 @@
 import axios from 'axios';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 export default () => {
+  const [memberList, setMemberList] = useState([]);
   const getMemberList = async () => {
     try {
       const res = await axios.get('http://localhost:9999/member/list');
       console.log(res.data);
+      setMemberList(res.data.list);
     } catch (err) {
       console.log(err);
     }
@@ -15,17 +17,30 @@ export default () => {
   }, []);
 
   return (
-    <div>
-      <table>
+    <div className="container-lg">
+      <table className="table table-hover table-striped">
         <thead>
           <tr>
-            <th>아이디</th>
+            <th className="">아이디</th>
             <th>이름</th>
             <th>닉네임</th>
             <th>비고</th>
           </tr>
         </thead>
-        <tbody></tbody>
+        <tbody>
+          {memberList &&
+            memberList.map((item) => (
+              <tr>
+                <td>{item.id}</td>
+                <td>{item.userName}</td>
+                <td>{item.nickName}</td>
+                <td>
+                  <button className="btn  btn-primary">수정</button>
+                  <button className="btn btn-danger">삭제</button>
+                </td>
+              </tr>
+            ))}
+        </tbody>
       </table>
     </div>
   );
